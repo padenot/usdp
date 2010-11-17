8789fed0e03d631960b05cefb0c42afea647b50c
martiusweb <martiusweb@gmail.com>
2010-11-17 10:48:30 +0000

    1st work sur tapis

--- rsase/SGBag/Tapis.cpp
+++ rsase/SGBag/Tapis.cpp
@@ -5,56 +5,85 @@
 
 #include "Tapis.h"
 //Begin section for file Tapis.cpp
-//TODO: Add definitions that you want preserved
+using namespace std;
 //End section for file Tapis.cpp
 
 
 //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_9b-b4OsVEd-oy8D834IawQ?DEFCONSTRUCTOR"
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-Tapis::Tapis()
+Tapis::Tapis() : chariotConnecte(NULL)
 {
-    //TODO Auto-generated method stub
+    // Constructeur
 }
 
 //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_9b-b4OsVEd-oy8D834IawQ?DESTRUCTOR"
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 Tapis::~Tapis()
 {
-    //TODO Auto-generated method stub
+    // Destructeur
 }
 
 //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_JpFKsO5zEd-X2qSx1xpmxg"
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 void Tapis::maj()
 {
-    //TODO Auto-generated method stub
+    // si un chariot est connecté au tapis
+    if(chariotConnecte != NULL)
+    {
+        // Déroule le tapis (fait avancer les objets).
+        deroulerTapis();
+
+        // Pour chaque bagage sur le tapis
+        Bagage* b;
+        std::pair<double, double> positionActuelle = position();
+        for(int i = 0; i < bagage.size(); ++i)
+        {
+            b = this->bagage[i];
+            b->simulerDeplacement(positionActuelle.first*vitesse, positionActuelle.second*vitesse);
+
+            if(bagageEstSorti(b))
+                chariotConnecte->chargerBagage(b);
+        }
+    }
 }
 
 //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_wD2mgO5-Ed-Jn7v3SB1Zsg"
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 void Tapis::ajouterBagage(Bagage* bagageEntrant)
 {
-    //TODO Auto-generated method stub
+    // Connexion du bagage
+    bagage.push_back(bagageEntrant);
 }
 
 //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_4ChQwPDwEd-R6YEVT5cViQ"
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 void Tapis::deroulerTapis()
 {
-    //TODO Auto-generated method stub
+    // TODO What does it do ?
 }
 
 //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#__9W_YPD7Ed-R6YEVT5cViQ"
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 void Tapis::connecter(Chariot* chariot)
 {
-    //TODO Auto-generated method stub
+    // On sait qu'il ne peut pas y avoir d'autre chariot puisque le
+    // test est effectué niveau tronçon support
+    chariotConnecte = chariot;
 }
 
 //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#__aB14PIZEd-TbK1o_cJlKw"
 //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
 Troncon* Tapis::trouverObjectifImmediat()
 {
-    //TODO Auto-generated method stub
+    // TODO WTFDID?
     return 0;
+}
+
+/**
+ * Compare les coordonnées du bagage aux coordonnées du tronçon suivant.
+ */
+bool Tapis::bagageEstSorti(Bagage *bagage)
+{
+    // TODO
+    return false;
 }
--- rsase/SGBag/Tapis.h
+++ rsase/SGBag/Tapis.h
@@ -7,6 +7,7 @@
 #define TAPIS_H
 //Begin section for file Tapis.h
 //TODO: Add definitions that you want preserved
+#include <vector>
 //End section for file Tapis.h
 
 #include "ElementActif.h"
@@ -30,10 +31,9 @@ class Tapis : ElementActif
     //End section for Tapis
 
     private:
-
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_tziQ8eshEd-oy8D834IawQ"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
-        Bagage * bagage;
+        std::vector<Bagage*> bagage;
 
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_fSJGQeswEd-oy8D834IawQ"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
@@ -43,39 +43,71 @@ class Tapis : ElementActif
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         Troncon * tronconSupport;
 
-
-
     protected:
 
+        /**
+         * Met le tapis en action (déplace les bagages).
+         */
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_4ChQwPDwEd-R6YEVT5cViQ"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         void deroulerTapis();
 
+        /**
+         * Teste si le bagage bagage est sorti du tapis.
+         * @param[in] bagage Bagage à tester
+         */
+        bool bagageEstSorti(Bagage* bagage);
+
     public:
 
+        /**
+         * Constructeur de tapis.
+         */
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_9b-b4OsVEd-oy8D834IawQ?DEFCONSTRUCTOR"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         Tapis();
 
+        /**
+         * Destructeur de tapis.
+         */
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_9b-b4OsVEd-oy8D834IawQ?DESTRUCTOR"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         virtual ~Tapis();
 
+        /**
+         * Opération de mise à jour en cascade de l'objet.
+         */
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_JpFKsO5zEd-X2qSx1xpmxg"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         void maj();
 
+        /**
+         * Ajoute le bagage bagageEntrant sur le tapis.
+         * @param[in] bagageEntrant
+         */
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#_wD2mgO5-Ed-Jn7v3SB1Zsg"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         void ajouterBagage(Bagage* bagageEntrant);
 
+        /**
+         * Connecte un chariot au tapis.
+         * @param[in,out] chariot Chariot arrivant et attendant un bagage.
+         */
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#__9W_YPD7Ed-R6YEVT5cViQ"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         void connecter(Chariot* chariot);
 
+        /**
+         * @todo commenter la méthode
+         */
         //@uml.annotationsderived_abstraction="platform:/resource/usdp/ModeleStructurel.emx#__aB14PIZEd-TbK1o_cJlKw"
         //@generated "UML to C++ (com.ibm.xtools.transform.uml2.cpp.CPPTransformation)"
         Troncon* trouverObjectifImmediat();
+
+        /**
+         * Modifie la vitesse.
+         */
+        void modifierVitesse();
 
 };  //end class Tapis
 
