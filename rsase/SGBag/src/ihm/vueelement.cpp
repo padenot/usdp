afa89e4e83d4b88c4b4752b669a58c6471ffc913
martiusweb <martiusweb@gmail.com>
2010-11-20 14:29:59 +0000

    Gestion du menu contextuel dans vue élément

--- rsase/SGBag/src/ihm/vueelement.cpp
+++ rsase/SGBag/src/ihm/vueelement.cpp
@@ -1,6 +1,42 @@
 #include "vueelement.h"
 
-VueElement::VueElement()
+VueElement::VueElement(): contextMenu(0), contextMenuActionsList()
 {
     setFlags(QGraphicsItem::ItemIsSelectable);
+
+    /* Exemple : Ajouter des QAction* au menu
+    QAction* action = new QAction("Label", this);
+    QObject::connect(action, SIGNAL(triggered()), this, SLOT(slotPourMonAction()));
+    contextMenuActionsList.append(action);
+    //*/
+}
+
+VueElement::~VueElement()
+{
+    // Détruire les QAction*
+    QList<QAction*>::iterator it;
+    for(it = contextMenuActionsList.begin(); it != contextMenuActionsList.end(); ++it)
+    {
+        delete *it;
+    }
+
+}
+
+/**
+ * Affiche un menu contextuel si des actions ont été renseignées dans la liste
+ * contextMenuActionsList.
+ */
+void VueElement::contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+{
+    // Si des actions sont définies pour le menu contextuel
+    if(!contextMenuActionsList.empty())
+    {
+        // On a pas encore renseigné les actions, il faut les ajouter
+        if(contextMenu.actions().empty())
+            contextMenu.addActions(contextMenuActionsList);
+
+        // On affiche
+        // TODO il faut peut-être faire une conversion vers la "global pos"
+        contextMenu.exec(event->screenPos());
+    }
 }
--- rsase/SGBag/src/ihm/vueelement.h
+++ rsase/SGBag/src/ihm/vueelement.h
@@ -2,12 +2,33 @@
 #define VUEELEMENT_H
 
 #include <QGraphicsItem>
+#include <QGraphicsSceneContextMenuEvent>
+#include <QMenu>
+#include <QList>
+#include <QAction>
 
 class VueElement : public QGraphicsItem
 {
 public:
     VueElement();
+    ~VueElement();
 
+protected:
+    /**
+     * @var contextMenu Menu contextuel
+     */
+    QMenu contextMenu;
+
+    /**
+     * @var contextMenuActionsList Liste des actions du menu contextuel, doit être rempli dans le constructeur
+     */
+    QList<QAction*> contextMenuActionsList;
+
+    /**
+     * Event handler appelé quand l'utilisateur souhaite afficher le menu contextuel.
+     * @param *event Evenement utilisateur
+     */
+    void contextMenuEvent(QGraphicsSceneContextMenuEvent *event);
 };
 
 #endif // VUEELEMENT_H
