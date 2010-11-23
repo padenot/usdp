7c270e68714a623aad2814af5835422ab625f7f4
Maxime Gaudin <maximegaudin@Maxime-Gaudins-MacBook-Pro.local>
2010-11-23 22:51:40 +0100

    Ajout d'un script d'export des sources

---
+++ Rapport/scripts/exportSources.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+def getSourceFiles(srcPath, ext):
+	root = srcPath
+	sourceList = []
+	for path, subdirs, files in os.walk(root):
+		for name in files:
+			if name.endswith(ext) : sourceList.append((name, os.path.join(path, name)))
+	return sourceList
+
+def toLatex(sourceList):
+	output = ""
+	for s in sourceList:
+		n = s[0]
+		p = s[1]
+		output += "\\nCode{" + n + "}{" + p + "}\n";
+	return output
+	
+def main():
+	print toLatex(getSourceFiles(sys.argv[1], "cpp"))
+
+main()
